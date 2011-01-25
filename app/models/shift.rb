a71da7b4e2f5e3f61aa79c4c8bdb5cab2a52f6b7
Abraham Oshel <abraham.oshel@gmail.com>
2011-01-25 11:28:04 -0600

    added some validations and changed some styling on the index page, show page, and form

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,2 +1,11 @@
 class Shift < ActiveRecord::Base
+  validates :name, :email, :presence => true 
+  validates_format_of :email, :with => /apple\.com/, :message => "You must use your Apple Email"
+  validates_format_of :takingemail, :with => /apple\.com/, :allow_blank => true,  :message => "You must use your Apple Email"
+
+  #def validate
+  #  errors.add_to_base "For Taker\'s Email You must use your Apple Email" if !takingemail.blank?
+  #  errors.add_to_base "For Giver\'s Email You must use your Apple Email" if !email.blank?
+  #end
+    
 end
