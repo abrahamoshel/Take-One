2cfd4fc8c461a117e000d5b87653b55e39b7271b
Abraham Oshel <aaoshel@wiki.nma.private>
2011-02-15 16:38:36 -0600

    setting up Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "TakeOne"
+set :repository,  "git@github.com:abrahamoshel/Take-One.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "schedule.nma.private/"
+
+
+
+role :web, "schedule.nma.private"                          # Your HTTP server, Apache/etc
+role :app, "schedule.nma.private"                          # This may be the same as your `Web` server
+role :db,  "schedule.nma.private", :primary => true # This is where Rails migrations will run
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
