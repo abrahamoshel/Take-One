ccf3f0d74891067e528137cc9981c758a144c7f3
Abraham Oshel <aaoshel@wiki.nma.private>
2011-04-26 13:39:31 -0500

    first revision of FOH shift switch

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'nokogiri'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml'
+gem 'formtastic', '~> 1.1.0'
+gem 'jammit'
+gem 'jquery-rails', '>= 0.2.6'
+gem 'passenger'
+
+group :development do
+  gem 'capistrano-ext', '~> 1.2.1'
+end
---
+++ app/mailers/destroy_mailer.rb
@@ -0,0 +1,23 @@
+class DestroyMailer < ActionMailer::Base
+  ActionMailer::Base.sendmail_settings = {
+    :location       => '/usr/sbin/sendmail',
+    :arguments      => '-i'
+  }
+    
+  default :from => "NMA Schedule <nmaschedule@apple.com>"
+
+    def shift_destroy(shift)
+      @shift = shift
+      @url = "http://www.apple.com"
+      headers "Reply-to" => "nmaschedule@apple.com"
+      attachments.inline['scheduleTeam.png'] = File.read(Rails.root.join('public/images/scheduleTeam.png'))
+      @css = File.read(File.join(Rails.root, 'public', 'stylesheets', 'notifier.css')).gsub(/\/images\/([a-zA-Z\-\_]+\.jpg)/) do |match|
+          attachments.inline[$1] = File.read File.join(Rails.root, 'public', 'images', $1)
+          attachments.inline[$1].url
+      end
+        
+      mail(:to => [shift.email, shift.takingEmail, "nmaschedule@apple.com"],
+          :subject => "A Shift Deleted!")
+    end
+ 
+end
