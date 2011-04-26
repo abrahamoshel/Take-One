b845a6e4a073b03a319b4b58d5f0c1944b8d7aa3
Abraham Oshel <aaoshel@wiki.nma.private>
2011-04-26 13:39:31 -0500

    first revision of FOH shift switch

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Takeone::Application.routes.draw do
+  resources :shifts
+  root :to => "shifts#index"
+
+  match "/manager" => "shifts#manager"
+  match "/shifts/shift_taken/:id" => "shifts#shift_taken"
+
+end
