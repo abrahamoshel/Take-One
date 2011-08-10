e735962be38704edb92d456216525edb34272ae6
Abraham Oshel <abraham.oshel@gmail.com>
2011-08-10 17:43:28 -0500

    adding request mailer

---
+++ app/mailers/request_mailer.rb
@@ -0,0 +1,17 @@
+class RequestMailer < ActionMailer::Base
+
+  default :from => "NMA Schedule <nmaschedule@apple.com>"
+
+    def shift_destroy(request)
+      @request = request
+      @url = "http://www.apple.com"
+      headers "Reply-to" => "nmaschedule@apple.com"
+      attachments.inline['scheduleTeam.png'] = File.read(Rails.root.join('public/images/scheduleTeam.png'))
+      @css = File.read(File.join(Rails.root, 'public', 'stylesheets', 'notifier.css')).gsub(/\/images\/([a-zA-Z\-\_]+\.jpg)/) do |match|
+          attachments.inline[$1] = File.read File.join(Rails.root, 'public', 'images', $1)
+          attachments.inline[$1].url
+      end
+      mail(:to => [request.email, "oldschoolpreach@mac.com"],
+          :subject => "A New Request")
+    end
+end
