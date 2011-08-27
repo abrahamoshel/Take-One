9a0d74ddb5fd2d8219d01f726a98ee27cf363ab6
Abraham Oshel <abraham.oshel@gmail.com>
2011-08-27 11:53:22 -0500

    adding devise for authentication and also customizing the devise views. also doing some house cleaning in that I deleted some images I don't need.

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,4 +1,5 @@
 class ShiftsController < ApplicationController
+  before_filter :authenticate_employee!
   # GET /shifts
   # GET /shifts.xml
   def shift_taken
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Takeone::Application.routes.draw do
+  devise_for :employees, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register'}
+
   resources :holidays, :except => :index
 
   resources :requests, :except => :index
 
   resources :shifts
-  root :to => "splash#index"
+
+  devise_for :employees do
+    get '/', :to => "devise/sessions#new"
+  end
+
+  root :to => "devise/sessions#new"
+  match '/retail' => "splash#index", :as => :employee_root
 
   match "/manager" => "shifts#manager"
   match "admin" => "holidays#index"
