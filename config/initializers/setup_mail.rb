61c56aabc21b21723bbbf84b84bfa61340fbd8a7
Abraham Oshel <abraham.oshel@gmail.com>
2011-08-29 08:36:48 -0500

    adding emails

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+#email settings --
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'oldschoolpreach@gmail.com',
+  :password             => 'zxcvzxcv1',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
