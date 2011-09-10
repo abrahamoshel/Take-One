23955cb0920df9f78a302c6986779c055bb9ff7a
Abraham Oshel <abraham.oshel@gmail.com>
2011-09-10 15:06:19 -0500

    manager and employee login working

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,7 +5,7 @@ class Employee < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :userName
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :userName, :firstName, :lastName
 
   validates_format_of :email, :with => /apple\.com/, :message => "You must use your Apple Email"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Takeone::Application.routes.draw do
   devise_for :employees, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register'}
+  devise_for :managers, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register'}
 
   resources :holidays, :except => :index
 
   resources :requests, :except => :index
 
   resources :shifts
+
+  match '/god' => "shifts#manager", :as => :manager_root
 
   devise_for :employees do
     get '/', :to => "devise/sessions#new"
